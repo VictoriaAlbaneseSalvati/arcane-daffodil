6dc357f39612817364b8f55cfb3338bd28d6cd56
Victoria Albanese <AlbaneseVi14@gmail.com>
2025-08-10 14:21:13 -0400

    add SpeedState and a publisher to publish a geometry_msgs::msg::Twist message based on the SpeedState

--- include/speed_controller.hpp
+++ include/speed_controller.hpp
@@ -9,11 +9,15 @@
 #ifndef SPEED_CONTROLLER_H
 #define SPEED_CONTROLLER_H
 
+#include <map>
 #include <memory>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 
+using CommandVelocity = geometry_msgs::msg::Twist;
 using LaserScan = sensor_msgs::msg::LaserScan;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -24,19 +28,45 @@ using LaserScan = sensor_msgs::msg::LaserScan;
  *        (1) Subscribes to a LaserScan topic and picks out the closest 
  *            sensor reading so we can know how close the nearest obstacle 
  *            is at any given time. 
- *        (2) Logs the closest sensor reading.
+ *        (2) Based on how close the nearest obstacle is, set the speed state
+ *            to FULL_SPEED, SLOW, or STOP based on the requirements.
+ *        (3) Based on the speed state, publish a command velocity to set
+ *            the speed of the robot.
  */
 class SpeedController : public rclcpp::Node
 {
 public:
 
     SpeedController();
+
+    enum SpeedState 
+    {
+        FULL_SPEED,
+        SLOW,
+        STOP
+    };
+    std::unordered_map<SpeedState, std::string> speed_state_to_str = {
+        { SpeedState::FULL_SPEED, "FULL_SPEED" },
+        { SpeedState::SLOW, "SLOW" },
+        { SpeedState::STOP, "STOP" },
+    };
     
 private:
 
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Publisher<CommandVelocity>::SharedPtr publisher;
     rclcpp::Subscription<LaserScan>::SharedPtr subscription;
     float min_laser_dist_meters;
+    SpeedState speed_state;
 
+    const float CLOSE_LASER_DIST_METERS = 0.8;
+    const float TOO_CLOSE_LASER_DIST_METERS = 0.4;
+    const float FULL_SPEED_MPS = 5.0;
+    const float SLOW_SPEED_MPS = 2.0;
+    const float STOP_SPEED_MPS = 0.0;
+
+    void determine_speed_state();
+    void publish_CommandVelocity();
     void callback_LaserScan(const LaserScan & msg);
 };
 
--- src/speed_controller.cpp
+++ src/speed_controller.cpp
@@ -7,6 +7,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <algorithm>
+#include <chrono>
 #include <functional>
 
 #include "speed_controller.hpp"
@@ -21,11 +22,78 @@ using std::placeholders::_1;
 SpeedController::SpeedController()
 : Node("speed_controller")
 , min_laser_dist_meters(2.0)
+, speed_state(SpeedState::FULL_SPEED)
 {
+    this->timer = this->create_wall_timer(
+        std::chrono::milliseconds(500),
+        std::bind(&SpeedController::publish_CommandVelocity, this));
+    this->publisher = this->create_publisher<CommandVelocity>("robot/cmd_vel", 10);
     this->subscription = this->create_subscription<LaserScan>(
         "/robot/base_scan", 
         10, 
         std::bind(&SpeedController::callback_LaserScan, this, _1));
+}
+
+
+/**
+ * @brief Determine what the speed state should be based on the 
+ *        min_laser_dist_meters member and the rules defined by the 
+ *        constants in the speed controller and set the speed_state 
+ *        member accordingly. 
+ *
+ * @note I took "within x" to mean < x and not <= x, but if that 
+ *       assumption was incorrect all that would need to be done is 
+ *       change these < to <=.
+ */
+void 
+SpeedController::determine_speed_state() 
+{
+    if (this->min_laser_dist_meters < this->TOO_CLOSE_LASER_DIST_METERS) 
+    {
+        this->speed_state = SpeedState::STOP;
+    }
+    else if (this->min_laser_dist_meters < this->CLOSE_LASER_DIST_METERS) 
+    {
+        this->speed_state = SpeedState::SLOW;
+    }
+    else
+    {
+        this->speed_state = SpeedState::FULL_SPEED;
+    }
+
+    RCLCPP_INFO(
+        this->get_logger(), 
+        "Min Laser Dist: %.2f meters - Speed State: %s", 
+        this->min_laser_dist_meters,
+        this->speed_state_to_str[this->speed_state].c_str());
+}
+
+
+/**
+ * @brief Based on the speed_state member, determine the speed that the robot
+ *        should be going based on the constants defined in the speed controller
+ *        and publish the appropriate speed. 
+ */
+void 
+SpeedController::publish_CommandVelocity() 
+{
+    this->determine_speed_state();
+
+    CommandVelocity velocity;
+    switch(this->speed_state) 
+    {
+        case SpeedState::STOP:
+            velocity.linear.x = this->STOP_SPEED_MPS;
+            break;
+        case SpeedState::SLOW:
+            velocity.linear.x = this->SLOW_SPEED_MPS;
+            break;
+        case SpeedState::FULL_SPEED:
+            velocity.linear.x = this->FULL_SPEED_MPS;
+            break;
+    }
+
+    this->publisher->publish(velocity);
 }
 
 
@@ -47,10 +115,6 @@ SpeedController::callback_LaserScan(const LaserScan& msg)
     if (it != laser_ranges.end()) 
     {
         this->min_laser_dist_meters = *it;
-        RCLCPP_INFO(
-            this->get_logger(), 
-            "Min laser dist: %f meters", 
-            this->min_laser_dist_meters);
     }
     else 
     {
